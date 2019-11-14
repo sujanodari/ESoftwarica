c4d7d8b38d13e4959ae66c36ebbe650bee42f33b
sujanodari <odarisujan@gmail.com>
2019-11-14 16:16:16 +0545

    slight changes to ui design

--- app/src/main/java/com/sujan/esoftwarica/DashboardActivity.java
+++ app/src/main/java/com/sujan/esoftwarica/DashboardActivity.java
@@ -24,7 +24,7 @@ public class DashboardActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_addstudent, R.id.navigation_aboutus)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+      //  NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
 
